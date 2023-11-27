bae5d3f97ee1a6258d2d7305a5f3b4259bfd9dfc
Daniel Cohen <danielcoh@wix.com>
2023-11-27 14:48:11 +0200

    feat - prettier install

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import Sidebar from './components/sidebar'
-import { BrowserRouter} from 'react-router-dom'
-import styles from './app.module.css'
-
+import Sidebar from './components/sidebar';
+import { BrowserRouter } from 'react-router-dom';
+import styles from './app.module.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className={styles.container}>
-        <Sidebar />
-        <div className={styles.content}>
-        </div>
-      </div>
-    </BrowserRouter>
-  )
-};
+   return (
+      <BrowserRouter>
+         <div className={styles.container}>
+            <Sidebar />
+            <div className={styles.content}></div>
+         </div>
+      </BrowserRouter>
+   );
+}
 
-export default App
+export default App;
--- src/Components/sidebar.tsx
+++ src/Components/sidebar.tsx
@@ -1,20 +1,23 @@
-import { Link } from 'react-router-dom'
-import styles from './sidebar.module.css'
-import { SidebarData } from './data'
+import { Link } from 'react-router-dom';
+import styles from './sidebar.module.css';
+import { sidebardata } from './data';
 
 export default function Sidebar() {
-  return (
-    <ul className={styles.navbar}>
-      {SidebarData.map((item, index) => {
-        return (
-          <li key={index} className={styles.listItem}>
-            <Link to={item.path} draggable={false} className={styles.sidebarContent}>
-              <div className={styles.icon}>{item.icon}</div>
-              <span className={styles.text}>{item.title}</span>
-            </Link>
-          </li>
-        )
-      })}
-    </ul>
-  )
+   return (
+      <ul className={styles.navbar}>
+         {sidebardata.map((item, index) => {
+            return (
+               <li key={index} className={styles.listItem}>
+                  <Link
+                     to={item.path}
+                     draggable={false}
+                     className={styles.sidebarContent}>
+                     <div className={styles.icon}>{item.icon}</div>
+                     <span className={styles.text}>{item.title}</span>
+                  </Link>
+               </li>
+            );
+         })}
+      </ul>
+   );
 }
