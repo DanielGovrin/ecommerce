9ae691263a1b28b12c0258b16f6dfff345234a55
Daniel Cohen <danielcoh@wix.com>
2023-11-27 11:51:55 +0200

    fix - remove unneccesary imports, change file names

--- src/Components/sidebar.tsx
+++ src/Components/sidebar.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './sidebar.module.css'
 import { SidebarData } from './data'
-type Props = {}
 
-export default function Sidebar({ }: Props) {
+export default function Sidebar() {
   return (
     <ul className={styles.navbar}>
       {SidebarData.map((item, index) => {
         return (
           <li key={index} className={styles.listItem}>
-            <Link to={item.path} draggable={false}>
-              <div className={styles.sidebarContent}>
-                <div className={styles.icon}>{item.icon}</div>
-                <span className={styles.text}>{item.title}</span>
-              </div>
+            <Link to={item.path} draggable={false} className={styles.sidebarContent}>
+              <div className={styles.icon}>{item.icon}</div>
+              <span className={styles.text}>{item.title}</span>
             </Link>
           </li>
         )
