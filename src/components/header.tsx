a73ffc74fda8aad03000489fe236b23ec7ef0019
Daniel Cohen <danielcoh@wix.com>
2023-12-04 14:49:21 +0200

    fix: changed file names and moved the data to assets

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import styles from "./header.module.css"
+
+function Header() {
+    return (
+        <div className={styles.headerContainer}>
+            <div>
+                s
+            </div>
+        </div>
+    )
+}
+
+export default Header
