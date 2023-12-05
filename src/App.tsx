d3c117d5c25ad18320da21218e383d016c7f85de
Daniel Cohen <danielcoh@wix.com>
2023-12-05 12:36:36 +0200

    feat: add a new header component and css file for the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import styles from './app.module.css';
 import Sidebar from './components/sidebar';
+import Header from './components/header';
+
 
 function App() {
    return (
       <BrowserRouter>
          <div className={styles.container}>
+            <Header/>
             <Sidebar />
             <div className={styles.content}></div>
          </div>
