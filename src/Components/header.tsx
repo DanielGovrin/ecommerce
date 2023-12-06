3a370e5417eb9ea384f82e5b2aa2a10d76e50a89
Daniel Cohen <danielcoh@wix.com>
2023-12-06 14:37:59 +0200

    feat: added a search bar and a search button

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -4,16 +4,15 @@ function Header() {
     return (
         <div className={styles.headerContainer}>
 
-            <svg width="70%" height="70%" className={styles.logo}>
-                {/* Include the contents of your SVG file here */}
-                <image href="./public/vite.svg" width="100%" height="100%" />
-            </svg>
 
             <div className={styles.categories}>
 
             </div>
 
-            <div className={styles.rightside}>
+            <div className={styles.searchAndCart}>
+                <input type="search" className={styles.siteSearch} value="Search"/>
+                <button className={styles.searchButton}>Search</button>
+                <img src="./src/assets/cart.svg"  className={styles.cartLogo} />
 
             </div>
         </div>
