14d53aa8292b8c5d4cf72df4fe2e28d68115b439
Daniel Cohen <danielcoh@wix.com>
2023-12-06 14:43:21 +0200

    feat: added a search bar and a search button

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,6 +1,15 @@
+import { ChangeEventHandler, useState } from "react";
 import styles from "./header.module.css"
 
 function Header() {
+
+
+    const [searchWord, setSearchWord] = useState({searchWord:''});
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setSearchWord({ ...formData, [e.target.name]: e.target.value });
+    };
+
     return (
         <div className={styles.headerContainer}>
 
@@ -10,9 +19,9 @@ function Header() {
             </div>
 
             <div className={styles.searchAndCart}>
-                <input type="search" className={styles.siteSearch} value="Search"/>
+                <input type="search" className={styles.siteSearch} value="Search" onChange={handleChange} />
                 <button className={styles.searchButton}>Search</button>
-                <img src="./src/assets/cart.svg"  className={styles.cartLogo} />
+                <img src="./src/assets/cart.svg" className={styles.cartLogo} />
 
             </div>
         </div>
