cddead24d124ff2b50c1d14c2870725bc5ab945f
Daniel Cohen <danielcoh@wix.com>
2023-12-07 12:03:33 +0200

    feat: added a new search bar and a cart icon

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,38 +1,35 @@
-import { ChangeEventHandler, useState } from "react";
-import styles from "./header.module.css"
+import { ChangeEventHandler, useState } from 'react';
+import styles from './header.module.css';
 
 function Header() {
+    const [searchWord, setSearchWord] = useState('');
 
-
-    const [searchWord, setSearchWord] = useState("Search");
-
-    const handleChange:ChangeEventHandler = (event) => {
-        setSearchWord(event.target.value)
-    }
-
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        setSearchWord(event.target.value);
+    };
 
     return (
         <div className={styles.headerContainer}>
+            <img src="/src/assets/logo.svg" alt="Logo" className={styles.logo} draggable="false"/>
 
-
-            <div className={styles.categories}>
-
-            </div>
+            <div className={styles.categories}></div>
 
             <div className={styles.searchAndCart}>
-                <input 
-                type="search"
-                className={styles.siteSearch}
-                value={searchWord}
-                placeholder="Search"
-                onChange={handleChange}
-                />
-                <button className={styles.searchButton}>Search</button>
-                <img src="./src/assets/cart.svg" className={styles.cartLogo} />
+                <form className={styles.searchbarContainer}>
+                    <button type="submit" className={styles.magnifyGlass}></button>
+                    <input
+                        type="search"
+                        className={styles.siteSearch}
+                        value={searchWord}
+                        placeholder="Search..."
+                        onChange={handleChange}
+                    />
+                </form>
+                <img src="src/assets/shopping-bag.svg" className={styles.shoppingBagLogo} draggable="false" />
 
             </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
