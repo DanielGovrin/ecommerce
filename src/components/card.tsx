12d7753b6bd892993fc012d272e20802d33dd831
Daniel Cohen <danielcoh@wix.com>
2023-12-10 15:02:01 +0200

    fix: fixed the overall layout to be more generic

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,17 +5,20 @@ import AddToCart from './AddToCart';
 function Card({ id, mainTitle, secondaryTitle, image, price }: CardItem) {
    return (
       <div className={styles.cardContainer}>
-         <div className={styles.titles}>
-            <h1 className={styles.mainTitle}>{mainTitle}</h1>
-            <h3 className={styles.secondaryTitle}>{secondaryTitle}</h3>
-         </div>
+
          <img
             className={styles.img}
             src={image}
             alt={mainTitle}
             draggable={false}
          />
-         <h3 className={styles.price}>{price}</h3>
+         <div className={styles.listingContainer}>
+            <div className={styles.titles}>
+               <h3 className={styles.mainTitle}>{mainTitle}</h3>
+               <h5 className={styles.secondaryTitle}>{secondaryTitle}</h5>
+            </div>
+            <h3 className={styles.price}>{price}</h3>
+         </div>
          <AddToCart id={id} />
       </div>
    );
