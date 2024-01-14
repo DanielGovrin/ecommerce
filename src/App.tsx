11e037edd42df2c53b0fb477b976e22920e1445c
Daniel Cohen <danielcoh@wix.com>
2024-01-14 10:40:11 +0200

    fix: css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Pants } from './pages/pants';
 import { Shirts } from './pages/shirts';
 import { Blur } from './components/blur/blur';
 
+
 export const App = () => {
    return (
       <MemoryRouter>
