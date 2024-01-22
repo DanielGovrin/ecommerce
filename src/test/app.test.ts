8a13a3810e40d084f2c8f29591f25cfb78d26194
Daniel Cohen <danielcoh@wix.com>
2024-01-22 14:52:59 +0200

    fix: changed the description of the tests to be more informative

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -6,7 +6,7 @@ import { NavigationDriver } from '../test-kit/navigation-driver';
 import { CartDriver } from '../test-kit/cart-driver';
 import { areNumbersClose } from '../utility';
 
-describe('My tests', function () {
+describe('E-Commerce Tests', function () {
    let page: Page;
    let browser: Browser;
    let context: BrowserContext;
@@ -38,14 +38,14 @@ describe('My tests', function () {
    });
 
    describe('Sidebar Navigation', async () => {
-      it('should navigate to shirts url', async () => {
+      it('should navigate to the "Shirts" category page', async () => {
          const id = 'SHIRTS';
          await navigationDriver.clickOnLinkById(id);
          const currentUrl = navigationDriver.getPageUrl();
          expect(currentUrl).to.equal(`http://localhost:${8000}/Shirts`);
       });
 
-      it('should navigate to home page', async () => {
+      it('should navigate to the "Pants" category page', async () => {
          const id: string = 'PANTS';
          await navigationDriver.clickOnLinkById(id);
          const currentUrl: string = navigationDriver.getPageUrl();
@@ -53,14 +53,14 @@ describe('My tests', function () {
       });
    });
    describe('Cart tests', async () => {
-      it('Verify that clicking the cart icon opens the modal and clicking outside the modal closes it.', async () => {
+      it('should open and close the cart modal correctly.', async () => {
          await cartDriver.clickOnCart();
          expect(await page.getByTestId('bagModal').isVisible()).to.be.true;
          await cartDriver.clickOutsideCart();
          expect(await page.getByTestId('bagModal').isVisible()).to.be.false;
       });
 
-      it('Verify proper error message when adding to cart without selecting size, and no message appears when size is selected.', async () => {
+      it('should display an error message when adding to cart without selecting size, and undisplay when choosing size', async () => {
          const typeOfItemId: string = cartDriver.getRandomTypeOfItem();
          const itemId: string = cartDriver.getArbitraryItemFromData(typeOfItemId);
          await navigationDriver.clickOnLinkById(typeOfItemId);
@@ -74,7 +74,7 @@ describe('My tests', function () {
          expect(await cardLoacator.locator('[data-testid="choose size"]').isVisible()).to.be.false;
       });
 
-      it('Confirm that the total price shown in the modal matches the accurate price', async () => {
+      it('should confirm that the total price in the modal matches the accurate price', async () => {
          const totalPrice: number = await cartDriver.addSomeItemsToCart(Math.random() * 30 + 1);
          await cartDriver.clickOnCart();
          const numberShownText: string | null = await page.getByTestId('total-price').textContent();
@@ -82,7 +82,7 @@ describe('My tests', function () {
          expect(areNumbersClose(totalPrice, numberShown, 1)).to.be.true;
       });
 
-      it('Ensures the total price in the modal updates with changes in product quantity.', async () => {
+      it('should update the total price in the modal with changes in product quantity', async () => {
          await navigationDriver.clickOnLinkById('PANTS');
          const itemId: string = 'pants10';
          const firstPantsCardLocator: Locator = await cartDriver.getCard(itemId);
