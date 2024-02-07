8ce8ccd157d99f09d173e69372ccaa40aab139c8
Daniel Cohen <danielcoh@wix.com>
2024-02-07 10:30:44 +0200

    name changed

--- src/_codux/boards/1.board.tsx
+++ src/_codux/boards/1.board.tsx
@@ -1,7 +1,7 @@
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: '1',
-    Board: () => <div></div>,
-    isSnippet: true,
+   name: '124124124',
+   Board: () => <div></div>,
+   isSnippet: true,
 });
